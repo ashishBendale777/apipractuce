4abded3bbd5a9f4661f9770f3e5a9bd67a46f32e
ashishBendale777 <36222108+ashishBendale777@users.noreply.github.com>
2023-12-12 20:19:32 +0530

    deone

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
 const express = require("express")
+const bodyparser = require("body-parser")
 
 const server = new express()
+server.use(bodyparser.json())
 
 server.get("/", (req, res) => {
     res.send("hello...")
 })
 
-server.get("/allusers",(req,res)=>{
-    const users=["Ashish","Swati"]
+server.get("/allusers", (req, res) => {
+    const users = ["Ashish", "Swati"]
     res.send(users)
 })
 
-server.listen(5000, () => { 
+server.post('/adduser', (req, res) => {
+    const userName = req.body.uname
+    const userPass = req.body.password
+
+    console.log(userName, userPass)
+    res.send("OK")
+})
+
+server.listen(5000, () => {
     console.log("Sercer Started")
 })
