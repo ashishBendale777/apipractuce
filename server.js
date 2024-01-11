2850963a763d3e29a0e1a8b4335f176ed3a10cf8
ashishBendale777 <36222108+ashishBendale777@users.noreply.github.com>
2024-01-11 20:11:02 +0530

    done

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require("express")
 const bodyparser = require("body-parser")
 const mongoose = require("mongoose")
 
+const multer = require('multer')
+const path = require('path')
+
+
+
 const server = new express()
 server.use(bodyparser.json())
 
@@ -12,6 +17,35 @@ mongoose.connect("mongodb://127.0.0.1:27017/practdb", {
 }).catch((err) => {
     console.log("Not Connected")
 });
+
+//storage config
+const fileStorage = multer.diskStorage({
+    destination: 'Uploads',
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname + "_" + Date.now() + path.extname(file.originalname))
+    }
+})
+
+//upload config
+const uploadConfig = multer({
+    storage: fileStorage,
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(png|jpg|jpeg)$/)) {
+            return cb(new Error('Upload Correct file'))
+        }
+        cb(undefined, true)
+    }
+})
+
+server.post('/uploadfile', uploadConfig.single('image'), (req, res) => {
+    res.status(200).json({
+        filepath: "/images/".concat(req.file.filename),
+        uploaded: true
+    })
+}, (err, req, res, next) => {
+    res.status(400).send({ error: err.message })
+})
+
 
 server.get("/", (req, res) => {
     res.send("hello...")
@@ -33,6 +67,10 @@ server.post('/adduser', (req, res) => {
 //setting routes
 const routes = require("./Routes/Routes")
 server.use("/api/", routes)
+
+server.use(express.static("Uploads"));
+server.use("/images", express.static("Uploads"));
+
 
 server.listen(5000, () => {
     console.log("Sercer Started")
